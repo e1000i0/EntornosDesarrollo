be157218a7b9edbb4721ffd90f4c220d8585cbc0
e1000ioCasa <emilioruizv7@gmail.com>
2024-11-25 22:10:34 +0100

    Main

---
+++ ProyectoEgit/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+//Creamos el programa principal main
+        Scanner sc = new Scanner(System.in);
+        int numero1, numero2, resultado;
+        System.out.print("Introduce primer número: ");
+        numero1 = sc.nextInt();
+        System.out.print("Introduce segundo número: ");
+        numero2 = sc.nextInt();
+    }
+}
